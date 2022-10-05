9e063aba1d998fe71dd6cb9a98e107a76ffd625b
edenyul97 <42135798+edenyul97@users.noreply.github.com>
2022-10-06 08:51:12 +0900

    Add files via upload

---
+++ CI_Unitest/CI_Unitest.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace CIUnitest
+{
+	TEST_CLASS(CIUnitest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			
+		}
+	};
+}
